a2a7d6e64a61c11f076f8c0547774bb98e3a87fa
supergoodname77 <epascuales@outlook.com>
2026-01-19 20:41:07 -0600

    disko

---
+++ disko-config.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:{
+  disko.imageBuilder.qemu = (import pkgs.path { system = "x86_64-linux"; }).qemu + "/bin/qemu-system-aarch64";
+  disko.devices = {
+    disk = {
+      main = {
+        # CFor SD cards, this is usually /dev/mmcblk0
+        device = "/dev/mmcblk0"; 
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            firmware = {
+              size = "512M";
+              type = "EF00"; 
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot/firmware";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
