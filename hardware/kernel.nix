7a484f31a37fa3343cd357060492544e185c9ae8
supergoodname77 <epascuales@outlook.com>
2026-02-13 21:53:55 -0600

    see if this quites brcmf warnings

--- hardware/kernel.nix
+++ hardware/kernel.nix
@@ -9,6 +9,14 @@ in {
   boot.loader.raspberryPi.firmwarePackage = kernelBundle.raspberrypifw;
   boot.kernelPackages = kernelBundle.linuxPackages_rpi5;
 
+  # Note this should be disabled eventually if brcmfmac is suspected to cause a problem
+  boot.kernelParams = [ 
+    "quiet" 
+    "loglevel=3"        # Only show Errors and above, hide Warnings/Info
+    "systemd.show_status=auto" 
+    "rd.udev.log_level=3" 
+  ];
+
   nixpkgs.overlays = [
     rust-overlay.overlays.default
     (self: super: {
