1d1dbf8642cae149d256ebd3769d1e92e3b940ea
supergoodname77 <epascuales@outlook.com>
2026-05-10 16:08:11 -0500

    Working ethernet bridge

--- modules/config/ethernet-bridge.nix
+++ modules/config/ethernet-bridge.nix
@@ -1,19 +1,31 @@
-{ lib, config, pkgs, ... }: { # Set a static IP on the "downstream" interface
+{ lib, config, pkgs, ... }: 
+{ 
+  # Set a static IP on the "downstream" interface
   networking.interfaces."end0" = {
     useDHCP = false;
     ipv4.addresses = [{
-      address = "10.0.0.1";
+      address = "192.168.50.1"; 
       prefixLength = 24;
     }];
   };
-  networking.firewall.extraCommands = ''
-    # Set up SNAT on packets going from downstream to the wider internet
-    iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 
-    # Accept all connections from downstream. May not be necessary
-    iptables -A INPUT -i enp2s0 -j ACCEPT
-  '';
-  # Run a DHCP server on the downstream interface
+  networking.firewall.interfaces."end0" = {
+    allowedTCPPorts = [ 22 ];   # Allow SSH
+    allowedUDPPorts = [ 67 ];   # Allow DHCP requests
+  };
+
+  networking.nat = {
+    enable = true;
+    internalInterfaces = [ "end0" ];
+    externalInterface = "wlan0";
+  };
+
+  # Enable packet forwarding in the kernel
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
+
+  # Run the DHCP server on the downstream interface
   services.kea.dhcp4 = {
     enable = true;
     settings = {
@@ -27,13 +39,13 @@
       renew-timer = 1000;
       subnet4 = [{
         id = 1;
-        pools = [{ pool = "10.0.0.2 - 10.0.0.255"; }];
-        subnet = "10.0.0.1/24";
+        pools = [{ pool = "192.168.50.2 - 192.168.50.255"; }];
+        subnet = "192.168.50.0/24";
       }];
       valid-lifetime = 4000;
       option-data = [{
         name = "routers";
-        data = "10.0.0.1";
+        data = "192.168.50.1";
       }];
     };
   };
